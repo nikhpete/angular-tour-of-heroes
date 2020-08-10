b2e0e2d122a887d4ee80202842d9e548bed42997
Nikhil Joseph Peter <josef.nikhil18@gmail.com>
2020-08-10 21:16:45 +0530

    erroe handling

--- src/app/components/heroes/service/hero.service.ts
+++ src/app/components/heroes/service/hero.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Hero } from '../../../model/hero';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { MessageService } from '../../messages/service/message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class HeroService {
@@ -10,17 +11,36 @@ export class HeroService {
   constructor(private http: HttpClient, private msgSvc: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
-    this.log('fetched heroes');
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
-    this.log(`fetched hero id =${id}`);
     const url = `${this.heroesUrl}/${id}`;
-    return this.http.get<Hero>(url);
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`fetched hero id =${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   private log(message: string) {
     this.msgSvc.add(`HeroService: ${message}`);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      this.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
